d278aa1d2f560d5584aa11fefe0ee076779ad64a
Claudio Costa <claudio@imaginaction.com.br>
2019-10-07 20:11:47 -0300

    feet: 01 - 06 - Criando a classe para o componente resumo

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,12 +3,11 @@ package br.com.alura.financask.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.alura.financask.R
-import br.com.alura.financask.extension.formataParaBrasileiro
 import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
+import br.com.alura.financask.ui.ResumoView
 import br.com.alura.financask.ui.adapter.ListaTrasacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
-import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 import java.util.*
 
@@ -18,20 +17,15 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
         val transacoes: List<Transacao> = trasacoesDeExemplo()
-
-        adicionaReceitaNoResumo(transacoes)
-
+        configuraResumo(transacoes)
         configuraLista(transacoes)
     }
 
-    private fun adicionaReceitaNoResumo(transacoes: List<Transacao>) {
-        var totalReceita = BigDecimal.ZERO;
-        for (transacao in transacoes) {
-            if (transacao.tipo == Tipo.RECEITA) {
-                totalReceita = totalReceita.plus(transacao.valor)
-            }
-        }
-        resumo_card_receita.text = totalReceita.formataParaBrasileiro()
+    private fun configuraResumo(transacoes: List<Transacao>) {
+        val view = window.decorView
+        val resumoView = ResumoView(view, transacoes)
+        resumoView.adicionaReceita()
+        resumoView.adicionaDespesa()
     }
 
     private fun configuraLista(transacoes: List<Transacao>) {
